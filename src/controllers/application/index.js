dd1d60f74d88722fc7908af65bfa2ab04b18b81d
SherSinghDev <shersingh741730@gmail.com>
2026-03-30 12:50:41 +0530

    rojgaar

--- src/controllers/application/index.js
+++ src/controllers/application/index.js
@@ -8,6 +8,7 @@ let { nanoid } = require('nanoid');
 const multer = require('multer');
 let bcrypt = require("bcrypt");
 const users = require('../../modals/users');
+const rojgaar = require('../../modals/rojgaar');
 
 // ===== MULTER CONFIGURATION =====
 const storage = multer.diskStorage({
@@ -80,6 +81,117 @@ router.post(
 
       // create a new application document
       let newApplication = new UserApplication({
+        name,
+        gender,
+        dateOfBirth,
+        relationType,
+        relationWith,
+        profession,
+        bloodGroup,
+        state,
+        district,
+        mobile,
+        aadharNo,
+        block,
+        village,
+        fullAddress,
+        pinCode,
+        email,
+        profilePicture,
+        idType,
+        idDocument,
+        otherDocument,
+        membershipType,
+        referredBy,
+        referralCode,
+        role: "Coordinator",
+        referrerName,
+        type,
+        position,
+        payment: {
+          mode: paymentMode,
+          receiptUrl
+        }
+      });
+
+      await newApplication.save();
+
+      res.status(201).json({
+        created: true,
+        message: 'Application submitted successfully!',
+        data: newApplication
+      });
+    } catch (error) {
+      console.error('Error saving user application:', error);
+      res.status(500).json({
+        created: false,
+        // message: 'Server error while submitting application.',
+        message: error.message,
+        error: "Error in Server"
+      });
+    }
+  }
+);
+
+// ===== POST ROUTE =====
+router.post(
+  '/rojgaar/apply',
+  upload.fields([
+    { name: 'profilePicture', maxCount: 1 },
+    { name: 'idDocument', maxCount: 1 },
+    { name: 'otherDocument', maxCount: 1 },
+    { name: 'receiptUrl', maxCount: 1 }
+  ]),
+  async (req, res) => {
+    try {
+      // console.log(req.body);
+
+      let {
+        name,
+        gender,
+        dateOfBirth,
+        relationType,
+        relationWith,
+        profession,
+        bloodGroup,
+        state,
+        district,
+        mobile,
+        aadharNo,
+        block,
+        village,
+        fullAddress,
+        pinCode,
+        // role,
+        email,
+        idType,
+        membershipType,
+        referredBy,
+        paymentMode,
+        referrerName,
+        type,
+        position
+      } = req.body;
+
+      // ✅ Check if email already exists in Users collection
+      const existingUser = await rojgaar.findOne({ email: email.toLowerCase().trim() });
+      if (existingUser) {
+        return res.status(400).json({
+          created: false,
+          message: 'This email is already registered in a Form. Please use a different email or contact support.'
+        });
+      }
+
+      // extract file paths safely
+      const profilePicture = req.files['profilePicture'] ? `/uploads/documents/${req.files['profilePicture'][0].filename}` : null;
+      const idDocument = req.files['idDocument'] ? `/uploads/documents/${req.files['idDocument'][0].filename}` : null;
+      const otherDocument = req.files['otherDocument'] ? `/uploads/documents/${req.files['otherDocument'][0].filename}` : null;
+      const receiptUrl = req.files['receiptUrl'] ? `/uploads/documents/${req.files['receiptUrl'][0].filename}` : null;
+
+      let referralCode = "TL-" + nanoid(6).toUpperCase()
+
+      // create a new application document
+      let newApplication = new rojgaar({
         name,
         gender,
         dateOfBirth,
@@ -283,6 +395,154 @@ router.get('/applied', async (req, res) => {
   }
 });
 
+// GET all applications
+router.get('/rojgaar/applied', async (req, res) => {
+  if (req.session.userId) {
+    try {
+      let user = await Users.findOne({ _id: req.session.userId })
+      // const applications = await UserApplication.find().sort({ createdAt: -1 });
+      let result;
+      if (user.role == "Coordinator") {
+        result = await rojgaar.aggregate([
+          {
+            $sort: { createdAt: -1 } // latest first
+          },
+          {
+            $match: { approveStatus: "Pending", parentUser: user._id }
+          },
+          {
+            $lookup: {
+              from: "users",              // collection name (must match in lowercase plural)
+              localField: "parentUser",   // field in UserApplication
+              foreignField: "_id", // field in User
+              as: "referrer",             // alias for matched user
+            },
+          },
+          {
+            $unwind: {
+              path: "$referrer",
+              preserveNullAndEmptyArrays: true, // keep even if no referrer found
+            },
+          },
+          {
+            $project: {
+              // keep all original fields
+              name: 1,
+              gender: 1,
+              dateOfBirth: 1,
+              relationType: 1,
+              relationWith: 1,
+              profession: 1,
+              bloodGroup: 1,
+              state: 1,
+              district: 1,
+              mobile: 1,
+              role: 1,
+              aadharNo: 1,
+              block: 1,
+              village: 1,
+              fullAddress: 1,
+              pinCode: 1,
+              email: 1,
+              profilePicture: 1,
+              idType: 1,
+              approveStatus: 1,
+              idDocument: 1,
+              otherDocument: 1,
+              membershipType: 1,
+              referredBy: 1,
+              referrerName: 1,
+              type: 1,
+              position: 1,
+              payment: 1,
+              createdAt: 1,
+              // only specific fields from referral user
+              'referrer._id': 1,
+              'referrer.name': 1,
+            }
+          },
+        ]);
+      }
+      else {
+        result = await rojgaar.aggregate([
+          {
+            $sort: { createdAt: -1 } // latest first
+          },
+          {
+            $match: { approveStatus: "Pending" }
+          },
+          {
+            $lookup: {
+              from: "users",              // collection name (must match in lowercase plural)
+              localField: "parentUser",   // field in UserApplication
+              foreignField: "_id", // field in User
+              as: "referrer",             // alias for matched user
+            },
+          },
+          {
+            $unwind: {
+              path: "$referrer",
+              preserveNullAndEmptyArrays: true, // keep even if no referrer found
+            },
+          },
+          {
+            $project: {
+              // keep all original fields
+              name: 1,
+              gender: 1,
+              dateOfBirth: 1,
+              relationType: 1,
+              relationWith: 1,
+              profession: 1,
+              bloodGroup: 1,
+              state: 1,
+              district: 1,
+              mobile: 1,
+              role: 1,
+              aadharNo: 1,
+              block: 1,
+              village: 1,
+              fullAddress: 1,
+              pinCode: 1,
+              email: 1,
+              profilePicture: 1,
+              idType: 1,
+              approveStatus: 1,
+              idDocument: 1,
+              otherDocument: 1,
+              membershipType: 1,
+              referredBy: 1,
+              referrerName: 1,
+              type: 1,
+              position: 1,
+              payment: 1,
+              createdAt: 1,
+              // only specific fields from referral user
+              'referrer._id': 1,
+              'referrer.name': 1,
+            }
+          },
+        ]);
+      }
+
+
+      // console.log(result);
+      let users = await Users.find({ role: "Coordinator" }).select('name referralCode userId -_id');
+
+
+      res.render('rojgaarsathi', { applications: result, page: "Rojgaar Sathi", user, users });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/auth/login')
+    }
+  }
+  else {
+    res.redirect('/auth/login')
+  }
+});
+
+
+
 // GET single application (for View Modal)
 router.get('/one/:id', async (req, res) => {
   try {
@@ -380,7 +640,17 @@ router.delete('/delete/:id', async (req, res) => {
 });
 
 
-router.get('/:type', async (req, res) => {
+router.delete('/rojgaar/delete/:id', async (req, res) => {
+  try {
+    await rojgaar.findByIdAndDelete(req.params.id);
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+
+router.get('/type/:type', async (req, res) => {
   let users = await Users.find({ role: "Coordinator" }).select('name referralCode userId -_id');
   let type = req.params.type
   console.log(type);
@@ -388,7 +658,14 @@ router.get('/:type', async (req, res) => {
   res.render('applicationform', { users, user, type })
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/rojgaar', async (req, res) => {
+  let type = 'singleorder'
+  res.render('rojgaar', { type })
+})
+
+
+
+router.get('/user/:id', async (req, res) => {
   let user = await Users.findOne({ userId: req.params.id }).select('name userId -_id');
   // console.log(user);
   let users = ''
